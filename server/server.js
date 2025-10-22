574ad85a5c46bbceda7e0f2cb381f995c7e06a21
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-10-22 06:55:33 +0300

    create mongoDB connect, server and user model

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import "dotenv/config"
+import connectDB from './configs/db.js'
+
+const app = express()
+
+// Connect to MongoDB
+await connectDB()
+
+//  Middleware
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
